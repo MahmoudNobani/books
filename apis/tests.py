753af2904b126731d20742426406eff9fbd8d2fd
MahmoudNobani <mahmoudnobani88@gmail.com>
2023-10-16 16:14:36 +0300

    third commit

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from books.models import Book
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_api_listview(client):
+    book = Book.objects.create(
+        title="Django for APIs",
+        subtitle="Build web APIs with Python and Django",
+        author="William S. Vincent",
+        isbn="9781735467221",
+    )
+
+    response = client.get(reverse("book_list"))
+    assert response.status_code == status.HTTP_200_OK
+    assert Book.objects.count() == 1
+    assert str(book) in str(response.content)
+
